9c577233ba9751bc9ebff1f21727c2a9bb3a6606
Laviest <laviest6@gmail.com>
2023-09-20 17:18:14 +0200

    Changed name and unique key error

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
             <div className='md:float-left block'>
                 <Link href="/">
                     <span className='cursor-pointer font-bold text-4xl text-white'>
-                        GraphCMS
+                        The Joshev Letter
                     </span>
                 </Link>
             </div>
